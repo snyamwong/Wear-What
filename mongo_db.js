0bb81f48eb9a2bfdfcbe285c88b720c002e45b12
Tin Wong <wongt1@wit.edu>
2019-02-15 16:22:57 -0500

    fixed deprecation warning, URL string parser

--- mongo_db.js
+++ mongo_db.js
@@ -1,8 +1,11 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/mydb";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
+MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
+    if (err) {
+        throw err;
+    } else {
+        console.log("Database created!");
+        db.close();
+    }
 });
