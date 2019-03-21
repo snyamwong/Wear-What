98a8609bfe2eaf77617f53628f881eca3a24a799
Tin Wong <wongt1@wit.edu>
2019-03-21 16:48:01 -0400

    form validation stuff and scrolling events

--- js/index.js
+++ js/index.js
@@ -1,45 +1,57 @@
-function validateForm(evt) {
-    function validateGender(gender_radio_group) {
-        for(var i = 0; i < gender_radio_group.length; i++) {
-            if(gender_radio_group[i].checked) {
-                return
-            }
-        }
+function init() {
+    // Since you can't get element by type, I use querySelectorAll to get all submit type input
 
-        var row = gender_radio_group[0].parentElement.parentElement.parentElement.parentElement.parentElement
-    }
+    // Since there is only one submit button, we just access index 0
+    btn_submit[0].addEventListener('click', validateForm, false)
 
-    function validateAttire(attire_radio_group) {
-        for(var i = 0; i < attire_radio_group.length; i++) {
-            if(attire_radio_group[i].checked) {
-                return
-            }
-        }
-
-        var row = attire_radio_group[0].parentElement.parentElement.parentElement.parentElement.parentElement
-    }
-
-    function validateLocation(location) {
-        for(var i = 0; i < location.length; i++) {
-            if(attire_radio_group[i].checked) {
-                return
-            }
-        }
-
-        var ro = location[0].parentElement.parentElement.parentElement.parentElement.parentElement
-    }
-
-    var gender_radio_group = document.getElementsByName('gender')
-    var attire_radio_group = document.getElementsByName('attire')
-    var location = document.getElementsByName('location')
-
-    validateGender(gender_radio_group)
-    validateAttire(attire_radio_group)
-    validateLocation(location)
+    createListenersForForm(gender_row, gender_radio_group)
+    createListenersForForm(attire_row, attire_radio_group)
 }
 
-// Since you can't get element by type, I use querySelectorAll to get all submit type input
-var btn_submit = document.querySelectorAll('input[type=submit]')
+function validateForm(evt) {
+    function validateRadio(radio_group, row) {
+        for(var i = 0; i < radio_group.length; i++) {
+            if(radio_group[i].checked) {
+                row.style.border = 'none'
+                return
+            }
+        }
 
-// Since there is only one submit button, we just access index 0
-btn_submit[0].addEventListener('click', validateForm, false)
+        row.style.border = 'thin solid red'
+    }
+
+    function validateText(text, row) {
+        for(var i = 0; i < location.length; i++) {
+            if(text[i].value == '' || text[i].value == null) {
+                row.style.border = 'thin solid red'
+                return
+            }
+        }
+
+        row.style.border = 'none'
+    }
+
+    var location_row = document.getElementById('location-row')
+    var location = document.getElementsByName('location')
+
+    validateRadio(gender_radio_group, gender_row)
+    validateRadio(attire_radio_group, attire_row)
+    validateText(location, location_row)
+}
+
+function createListenersForForm(row, elements) {
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', () => {
+            var next_row = row.nextElementSibling
+            next_row.scrollIntoView()
+        })
+    }
+}
+
+var btn_submit = document.querySelectorAll('input[type=submit]')
+var gender_row = document.getElementById('gender-row')
+var gender_radio_group = document.getElementsByName('gender')
+var attire_row = document.getElementById('attire-row')
+var attire_radio_group = document.getElementsByName('attire')
+
+init()
