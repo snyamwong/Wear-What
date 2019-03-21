6f260a8c6c5e96e4883fdd0cd317dc7ba15a8b70
Tin Wong <wongt1@wit.edu>
2019-03-21 19:11:16 -0400

    implemented logic for querying for clothes

--- js/mongo_db.js
+++ js/mongo_db.js
@@ -1,20 +1,50 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
-    if (err) {
-        throw err;
-    } else {
-        var dbo = db.db("clothes");
+function queryMongoDB(gender, attire, temperature, conditions) {
+    MongoClient.connect(url, {useNewUrlParser: true}, function(err, db) {
+        if (err) {
+            throw err
+        } else {
+            var dbo = db.db("clothes")
+            var category = ''
 
-        var query = { attire: "business" };
-
-        dbo.collection("men").find(query).toArray(function(err, result) {
-            if (err) {
-                throw err;
+            if (temperature < 45) {
+                category += 'cold'
             }
-            console.log(result);
-            db.close();
-        });
-    }
-});
+            else if (temperature < 70) {
+                category += 'mild'
+            }
+            else {
+                category += 'hot'
+            }
+
+            if (conditions.includes('snow') ||
+                conditions.includes('rain') ||
+                conditions.includes('thunderstorms') ||
+                conditions.includes('showers') ||
+                conditions.includes('flurries') ||
+                conditions.includes('drizzle')) {
+                    category += '-wet'
+                }
+            else {
+                category += '-dry'
+            }
+
+            var query = {
+                attire: attire,
+                category: category,
+            }
+
+            dbo.collection(gender).find(query).toArray(function(err, result) {
+                if (err) {
+                    throw err
+                }
+
+                console.log('MongodDB Query: ' + result)
+
+                db.close()
+            });
+        }
+    });
+}
